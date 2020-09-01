e544dfcd67f01acc49fc4f8085f1c3a7502ed8c0
Rudi Victor <rudi@fluenty.co.za>
2020-09-01 23:22:06 +0200

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { json } from "express";
+
+const app = express();
+const port = 3030;
+
+app.use(json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello, Rudi" });
+});
+
+app.listen(port, () => {
+  console.log(`"Server running on port ${port}`);
+});
